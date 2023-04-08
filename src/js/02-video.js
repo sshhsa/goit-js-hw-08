0309bc51c10695ed160e992b07dfe120aaed73d7
Olexandr <118224486+sshhsa@users.noreply.github.com>
2023-04-08 16:42:57 +0300

    Second task is done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function onTimeUpdate(e) {
+  const currentTime = e.seconds;
+  localStorage.setItem('videoplayer-current-time', currentTime);
+}
+
+window.addEventListener('load', function () {
+  const savedTime = localStorage.getItem('videoplayer-current-time');
+  if (savedTime) {
+    player.setCurrentTime(savedTime);
+  }
+});
+
+iframe.addEventListener('click', function () {
+  player.play();
+});
